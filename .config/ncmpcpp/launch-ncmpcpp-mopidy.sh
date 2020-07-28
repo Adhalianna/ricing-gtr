4a718f67238b2e4b7c9e4db135fcc5459c72a746
Adhalianna <52415679+Adhalianna@users.noreply.github.com>
2020-07-29 00:43:35 +0200

    Add files via upload

---
+++ .config/ncmpcpp/launch-ncmpcpp-mopidy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mopidy &
+mkfifo /tmp/mpd.fifo
+while : 
+	do
+		yes $’\n’ | nc -lu 127.0.0.1 5555 > /tmp/mpd.fifo
+	done &
+sleep 5
+alacritty --class Ncmpcpp,Ncmpcpp -e ncmpcpp
